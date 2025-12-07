e3911ff83eb7544c0d1dcaa035c283f35e478750
Myakey <goldenation139@gmail.com>
2025-12-07 16:15:52 +0700

    Fixing bugs 1

--- client/src/pages/public/ProductDetails.jsx
+++ client/src/pages/public/ProductDetails.jsx
@@ -17,7 +17,7 @@ import ReviewStats from "../../components/review/ReviewStats";
 import ReviewFilters from "../../components/review/ReviewFilters";
 import reviewService from "../../services/reviewService";
 import { useCart } from "../../context/cartContext";
-import { getProductById } from "../../../../server/models/productsModel";
+import productService from "../../services/productService";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -46,7 +46,7 @@ const ProductDetails = () => {
     const fetchProduct = async () => {
       try {
         setLoading(true);
-        const res = await getProductById(id);
+        const data = await productService.getProductById(id);
         console.log("API Response:", data);
 
         const formattedProduct = {
